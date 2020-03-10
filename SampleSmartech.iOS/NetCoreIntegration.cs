a44f560924852c584c88eb208419d76b6d52f91b
Olamide james <olamidejames007@gmail.com>
2020-03-10 16:29:41 +0100

    * .gitignore: initial commit
    * SampleSmartech.sln:
    * App.xaml:
    * Main.cs:
    * App.xaml.cs:
    * AppShell.xaml:
    * Info.plist:
    * Item.cs:
    * AssemblyInfo.cs:
    * AppShell.xaml.cs:
    * AppDelegate.cs:
    * Smartech.dll:
    * ItemsPage.xaml:
    * AboutPage.xaml:
    * SampleSmartech.csproj:
    * Entitlements.plist:
    * NewItemPage.xaml:
    * INetCoreIntegration.cs:
    * IDataStore.cs:
    * AboutPage.xaml.cs:
    * ItemsPage.xaml.cs:
    * MainActivity.cs:
    * ItemDetailPage.xaml:
    * MockDataStore.cs:
    * Default.png:
    * NetCoreIntegration.cs:
    * NewItemPage.xaml.cs:
    * tab_feed.png:
    * BaseViewModel.cs:
    * tab_about.png:
    * ItemDetailPage.xaml.cs:
    * ItemsViewModel.cs:
    * AboutViewModel.cs:
    * Default@2x.png:
    * SampleSmartech.iOS.csproj:
    * tab_feed@3x.png:
    * tab_feed@2x.png:
    * tab_about@3x.png:
    * xamarin_logo.png:
    * tab_about@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * ItemDetailViewModel.cs:
    * xamarin_logo@3x.png:
    * xamarin_logo@2x.png:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * colors.xml:
    * Tabbar.xml:
    * styles.xml:
    * AboutResources.txt:
    * Toolbar.xml:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * SampleSmartech.Android.csproj:
    * icon.png:
    * icon.png:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * tab_feed.png:
    * icon.png:
    * icon.png:
    * tab_about.png:
    * icon.png:
    * xamarin_logo.png:
    * icon.xml:
    * Icon80.png:
    * Icon29.png:
    * Icon87.png:
    * Icon40.png:
    * Icon58.png:
    * Icon60.png:
    * Icon20.png:
    * Icon76.png:
    * Icon120.png:
    * icon_round.xml:
    * Icon167.png:
    * Icon152.png:
    * Icon180.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ SampleSmartech.iOS/NetCoreIntegration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using NetCorePush;
+using SampleSmartech.iOS;
+
+[assembly: Xamarin.Forms.Dependency(typeof(NetCoreIntegration))]
+namespace SampleSmartech.iOS
+{
+    public class NetCoreIntegration : INetCoreIntegration
+    {
+        public NetCoreIntegration()
+        {
+        }
+
+        public void Login(string userKey)
+        {
+            try
+            {
+                  NetCoreSharedManager.SharedInstance().SetUpIdentity(userKey);
+                  NetCoreInstallation.SharedInstance().NetCorePushLogin(userKey);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+       
+
+        public void TrackEvent(string email, string eventName, Dictionary<string, string> payLoad)
+        {
+           
+        }
+    }
+}
---
+++ SampleSmartech/INetCoreIntegration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleSmartech
+{
+    public interface INetCoreIntegration
+    {
+        /// <summary>
+        /// userKey could be email address
+        /// </summary>
+        /// <param name="userKey"></param>
+        void Login(string userKey);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="email">customer's email</param>
+        /// <param name="eventName">event to track</param>
+        /// <param name="payLoad">key value pair of parameters</param>
+        void TrackEvent(string email, string eventName, Dictionary<string, string> payLoad);
+    }
+}
